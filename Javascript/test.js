d901bb9c10d94c458704a26ad9706bf76866ac7f
Manibharathi <manibharath159@gmail.com>
2026-02-20 21:15:04 +0530

    Javascript Basics

---
+++ Javascript/test.js
@@ -0,0 +1,43 @@
+// console.log("I am there");
+
+let a  = 10;
+let b = 22;
+let c = a+b;
+console.log("The addition is ",c);
+
+// Variables
+// It is container or bucket -> which holds some value/thing
+//    a = 10;
+// It is a mutable
+// Data Type
+// let a = 10;
+// int a = 10;
+// String name = "Qtree"
+
+// let or var
+// let a = 10; -> ES6
+// var a = 10; -> ES5
+// Datatypes
+
+let firstNumber = 10; //Numeric
+let secondNumber = "20"; // Strings
+let result = firstNumber + secondNumber; // Concatenation
+console.log("The result is",result);
+console.log(typeof(secondNumber));
+
+let finalResult = firstNumber + parseInt(secondNumber);
+console.log("The final result is",finalResult);
+
+firstNumber = 45;
+secondNumber = 56;
+console.log(typeof(secondNumber));
+result = firstNumber + secondNumber;
+console.log("result",result);
+let my_salary = 250000;
+
+//Rules to be followed variable declarations
+// CamelCase Letter
+// Don't use digits in prefix ex; 2myName(myName)
+// we can use digitis between
+// we can use _
+// Don't put any spaces. If you want, use _
